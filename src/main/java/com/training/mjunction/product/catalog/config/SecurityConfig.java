075a7110a5ddac07c401b8b2dfc68f6eae7a8ad3
talus005 <sanjib.x.talukdar.-nd@disney.com>
2019-07-06 23:55:21 +0530

    updated

--- src/main/java/com/training/mjunction/product/catalog/config/SecurityConfig.java
+++ src/main/java/com/training/mjunction/product/catalog/config/SecurityConfig.java
@@ -1,6 +1,16 @@
 package com.training.mjunction.product.catalog.config;
 
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +25,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(final WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/actuator/**", "/js/**", "/css/**", "/*.html", "/*.htm", "/*.jsp",
 				"/swagger-ui.html", "/v2/**");
+	}
+
+	@Bean
+	@Order(-1111111)
+	public Boolean disableSSLValidation() throws Exception {
+
+		final SSLContext sslContext = SSLContext.getInstance("TLS");
+
+		sslContext.init(null, new TrustManager[] { new X509TrustManager() {
+			@Override
+			public void checkClientTrusted(final X509Certificate[] x509Certificates, final String s)
+					throws CertificateException {
+			}
+
+			@Override
+			public void checkServerTrusted(final X509Certificate[] x509Certificates, final String s)
+					throws CertificateException {
+			}
+
+			@Override
+			public X509Certificate[] getAcceptedIssuers() {
+				return new X509Certificate[0];
+			}
+		} }, null);
+
+		HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+		HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+
+		return true;
 	}
 
 }
